638cf2edcc67d6cf7c2377c57451f428c1621996
Aaron <aarontburnham@hotmail.com>
2024-04-15 19:55:25 -0700

    worked on volume controller

--- src/modules/volume_controller/VolumeControllerModule.ts
+++ src/modules/volume_controller/VolumeControllerModule.ts
@@ -2,12 +2,16 @@ import { Setting } from "../../module_builder/settings/Setting";
 import { Module } from "../../module_builder/Module";
 import { NodeAudioVolumeMixer } from "node-audio-volume-mixer";
 import * as path from "path";
+import { exec } from "child_process"
 
 
 export class VolumeControllerModule extends Module {
 
     private static MODULE_NAME = "Volume Controller";
     private static HTML_PATH: string = path.join(__dirname, "./VolumeControllerHTML.html").replace("dist", "src");
+
+    private static VOLUME_REFRESH_MS = 500;
+
 
     public constructor() {
         super(VolumeControllerModule.MODULE_NAME, VolumeControllerModule.HTML_PATH);
@@ -16,9 +20,18 @@ export class VolumeControllerModule extends Module {
     public initialize(): void {
         // Get a audio session.
 
+        exec('tasklist', function (err, stdout, stderr) {
+            const lines = stdout.toString().split('\n');
+            lines.forEach(function (line) {
+                const parts = line.split('=');
+                parts.forEach(items => {
+                    console.log(items);
+                })
+            });
+        });
 
         this.updateSessions();
-        setTimeout(() => this.updateSessions(), 1000);
+        setTimeout(() => this.updateSessions(), VolumeControllerModule.VOLUME_REFRESH_MS);
     }
 
     private updateSessions() {
@@ -27,10 +40,13 @@ export class VolumeControllerModule extends Module {
 
         const updatedSessions: { pid: number, name: string, volume: number }[] = [];
         sessions.forEach((session) => {
-            updatedSessions.push({ ...session, volume: NodeAudioVolumeMixer.getAudioSessionVolumeLevelScalar(session.pid) })
+            if (session.pid === 0) {
+                session.name = "System Volume"
+            }
+            updatedSessions.push({ ...session, volume: this.getProcessVolume(session.pid) })
         });
         this.notifyObservers("vol-sessions", ...updatedSessions);
-        setTimeout(() => this.updateSessions(), 1000);
+        setTimeout(() => this.updateSessions(), VolumeControllerModule.VOLUME_REFRESH_MS);
     }
 
     public registerSettings(): Setting<unknown>[] {
@@ -45,9 +61,41 @@ export class VolumeControllerModule extends Module {
                 this.initialize();
                 break;
             }
+            case "volume-modified": {
+                const sessionPID: number = Number(data[0]);
+                const newVolume: number = Number(data[1]) / 100;
+                console.log("PID: " + data[0] + " New Volume: " + data[1])
+                this.setProcessVolume(sessionPID, newVolume);
+                break;
+            }
 
         }
     }
+
+    private setMasterMuted(isMuted: boolean): void {
+        NodeAudioVolumeMixer.muteMaster(isMuted);
+    }
+
+    private setProcessVolume(pid: number, volume: number): void {
+        if (volume > 1 || volume < 0) {
+            console.log("ERROR (VolumeControllerModule): Volume out of range 0.0 - 1.0: " + volume + " for PID " + pid)
+            return
+        }
+        NodeAudioVolumeMixer.setAudioSessionVolumeLevelScalar(pid, volume);
+    }
+
+    private setMasterVolume(volume: number): void {
+        if (volume > 1 || volume < 0) {
+            console.log("ERROR (VolumeControllerModule): Volume out of range 0.0 - 1.0: " + volume + " for master")
+            return
+        }
+        NodeAudioVolumeMixer.setMasterVolumeLevelScalar(volume);
+    }
+
+    private getProcessVolume(pid: number): number {
+        return NodeAudioVolumeMixer.getAudioSessionVolumeLevelScalar(pid);
+    }
+
 
 
 }
--- src/modules/volume_controller/VolumeControllerRenderer.ts
+++ src/modules/volume_controller/VolumeControllerRenderer.ts
@@ -6,32 +6,40 @@ window.parent.ipc.on("volume controller-renderer", (_, eventType: string, data: 
             const sessionArray: { pid: number, name: string, volume: number }[] = data;
 
             sessionArray.forEach(session => {
+                const roundedVolume: number = Math.round(session.volume * 100);
                 const existingHTML: HTMLElement = document.getElementById(`session-${session.pid}`);
                 if (existingHTML === null) {
-                    const html: string = `
-                    <div id='session-${session.pid}'>
-                        <p class='session-name'>${session.pid === 0 ? "System Volume" : session.name} (${session.pid})</p>
-                        <p class='session-volume'>${Math.round(session.volume * 100)}%</p>
-                    </div>
-                `
-                    document.getElementById("session-box").insertAdjacentHTML("beforeend", html);
+                    const parent: HTMLElement = document.createElement('div');
+                    parent.id = `session-${session.pid}`;
+                    parent.className = 'session-box'
+                    const innerHtml: string = `
+                        <p class='session-name'>${session.name} (${session.pid})</p>
+                        <p class='session-volume'>${roundedVolume}%</p>
+                        <input class='vol-slider' type="range" min="0" max="100" value='${roundedVolume}'>
+                    `
+                    parent.innerHTML = innerHtml;
+                    const slider: HTMLInputElement = parent.querySelector('.vol-slider') as HTMLInputElement;
+                    slider.addEventListener("input", (event: Event) => {
+                        window.ipc.send('volume controller-process', "volume-modified", session.pid, (event.target as HTMLInputElement).value);
+                    })
+
+                    document.getElementById("session-box-container").appendChild(parent);
+
                 } else {
                     for (let i = 0; i < existingHTML.children.length; i++) {
                         const child = existingHTML.children.item(i);
                         if (child.className === "session-name") {
-                            child.textContent = (session.pid === 0 ? "System Volume" : session.name) + ` (${session.pid})`;
+                            child.textContent = session.name + ` (${session.pid})`;
                         } else if (child.className === "session-volume") {
-                            child.textContent = `${Math.round(session.volume * 100)}%`;
+                            child.textContent = `${roundedVolume}%`;
                         }
 
                     }
                 }
             });
-
-
-
             break;
         }
     }
 });
+
 
