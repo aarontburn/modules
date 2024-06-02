abb8bc06634ba47a06cd0bc7da3dfb11307995dd
Aaron <aarontburnham@hotmail.com>
2024-06-01 21:35:47 -0700

    Update renderer.ts

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,10 +2,27 @@
     const PROCESS: string = "main-process";
     const RENDERER: string = "main-renderer";
 
+    window.ipc.send(PROCESS, "renderer-init"); // let main know that renderer is booted
+
     const IFRAME_DEFAULT_STYLE: string = "height: 100%; width: 100%;";
+
+    // const SANDBOX_RESTRICTIONS: string = Array.from(new Map([
+    //     ["allow-forms", true], // Module can submit forms.
+    //     ['allow-modals', true],  // Module can spawn alerts, prompts, and confirms
+    //     ['allow-orientation-lock', false],
+    //     ['allow-pointer-lock', false], // Module can 
+    //     ['allow-popups', true],
+    //     ['allow-popups-to-escape-sandbox', true],
+    //     ['allow-presentation', false],
+    //     ['allow-same-origin', true],
+    //     ['allow-scripts', true],
+    //     ['allow-top-navigation', false],
+    //     ['allow-top-navigation-by-user-activation', false],
+    // ])).filter(([_, v]) => v).reduce((prev, [k, _]) => prev += `${k} `, "");
+
+
     let selectedTab: HTMLElement = undefined;
 
-    window.ipc.send(PROCESS, "renderer-init"); // let main know that renderer is booted
 
     window.ipc.on(RENDERER, (_, eventType: string, data: any) => {
         // data = data[0];
@@ -19,24 +36,25 @@
                     moduleView.id = moduleName;
                     moduleView.setAttribute("src", moduleHtmlPath);
                     moduleView.setAttribute("style", IFRAME_DEFAULT_STYLE);
+                    // moduleView.setAttribute("sandbox", SANDBOX_RESTRICTIONS)
                     moduleHtml.insertAdjacentElement("beforeend", moduleView);
 
-                    const id: string = moduleName + "HeaderButton";
+
                     const headerButton: HTMLElement = document.createElement("button");
-                    headerButton.id = id;
+                    headerButton.id = moduleName + "HeaderButton";
                     headerButton.textContent = moduleName;
 
-                    if (moduleName == "Home") {
-                        headerButton.setAttribute("style", "color: var(--accent-color);")
+                    if (moduleName === "Home") {
+                        headerButton.setAttribute("style", "color: var(--accent-color);");
                         selectedTab = headerButton;
                     }
 
                     headerButton.addEventListener("click", () => {
-                        if (selectedTab != undefined) {
+                        if (selectedTab !== undefined) {
                             selectedTab.style.color = "";
                         }
                         selectedTab = headerButton;
-                        selectedTab.setAttribute("style", "color: var(--accent-color);")
+                        selectedTab.setAttribute("style", "color: var(--accent-color);");
 
                         window.ipc.send(PROCESS, "swap-modules", moduleName);
                     });
@@ -52,7 +70,7 @@
     })
 
     function swapLayout(swapToLayoutId: string): void {
-        const modules: HTMLCollection = document.getElementById("modules").getElementsByTagName("*")
+        const modules: HTMLCollection = document.getElementById("modules").getElementsByTagName("*");
         for (let i = 0; i < modules.length; i++) {
             modules[i].setAttribute("style", IFRAME_DEFAULT_STYLE + "display: none;");
         }
