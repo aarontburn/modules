82c38c159b25ffc056fbad04088283ad3c2aed5d
Aaron <aarontburnham@hotmail.com>
2024-06-23 18:15:29 -0700

    fixed some stuff

--- copyFiles.js
+++ copyFiles.js
@@ -16,13 +16,31 @@ pathsToCopy.forEach(file => {
     console.log("Copying `" + file + "` to `" + file.replace("src", "dist") + "`")
 
     if (!file.includes(".")) {
-        fs.cpSync(file, file.replace("src", "dist"), {recursive: true})
+        fs.cpSync(file, file.replace("src", "dist"), { recursive: true })
         return;
     }
-    
+
     fs.copyFileSync(file, file.replace("src", "dist"))
 });
 
 
+if (process.argv.includes('--submodule')) {
+    function getDirectoriesRecursive(srcPath) {
+        const flatten = (lists) => lists.reduce((a, b) => a.concat(b), []);
+        const getDirectories = (parentDir) => fs.readdirSync(parentDir)
+            .map(file => path.join(parentDir, file))
+            .filter(path => fs.statSync(path).isDirectory())
+        return [srcPath, ...flatten(getDirectories(srcPath).map(getDirectoriesRecursive))];
+    }
+
+    const subfolders = getDirectoriesRecursive(`${PWD.replace('src', 'dist')}`);
+    for (const f of subfolders) {
+        const parents = f.split('\\');
+        if (parents.at(-1) === 'module_builder' && parents.at(-2) !== 'dist') {
+            fs.cpSync(f, PWD.replace('src', 'dist') + '/module_builder/', { recursive: true })
+            return;
+        }
+    }
+}
 
 
